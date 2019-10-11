add6694725a791bbd2d24e4b43e18777c52b7294
Liron Harari <lironharari@gmail.com>
2019-10-11 18:23:42 -0500

    zoom bug

--- client/src/components/ImageGallery.jsx
+++ client/src/components/ImageGallery.jsx
@@ -47,6 +47,7 @@ export default function ImageGallery({ photos }) {
                 <Carousel 
                         views={photos}
                         currentIndex={photoIndex}
+                        trackProps={{ swipe: false }}
                          />
               </Modal>
             ) : null}
