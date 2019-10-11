071dfb45b6e182ee50b7ba973a45b8bc68a1c9a8
Liron Harari <lironharari@gmail.com>
2019-10-11 01:10:19 -0500

    responsive

--- client/src/components/ImageGallery.jsx
+++ client/src/components/ImageGallery.jsx
@@ -47,13 +47,11 @@ export default function ImageGallery({ photos }) {
                 <Carousel 
                         views={photos}
                         currentIndex={photoIndex}
+                        trackProps={ { swipe: false }}
                          />
               </Modal>
             ) : null}
           </ModalGateway>                                
-
-
-
   </div>
   );
 }
