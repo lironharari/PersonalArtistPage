4919527482b8b819a6367c628a43a40b0c7eab51
Liron Harari <lironharari@gmail.com>
2019-10-11 03:44:39 -0500

    full screen

--- client/src/components/ImageGallery.jsx
+++ client/src/components/ImageGallery.jsx
@@ -40,7 +40,7 @@ export default function ImageGallery({ photos }) {
             {isOpen ? (
               <Modal 
                   onClose={() => setIsOpen(false)}
-                  allowFullscreen={false}
+                  allowFullscreen={true}
                   >
                 <Carousel 
                         views={photos}
