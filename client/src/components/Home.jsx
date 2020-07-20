69c462e7f2d87cea11d58295e0d2715132ca03d6
liron harari <lironharari@gmail.com>
2020-07-19 20:41:26 -0500

    update

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,75 +1,20 @@
 import React, { Component } from 'react';
 import Gallery from "react-photo-gallery";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import photos from './data/home.json'
 
-class Home extends Component {
-  
-    render() {
-      
-      const photos = [
-        {
-          src: "/images/thumbnails/_DSC3766.jpg",
-          width: 800,
-          height: 600,
-          title:"The Life and Death of Joe Shmoe - Drawings",
-          href:"drawings"
-        },                        
-        {
-          src: "/images/thumbnails/DSC_0027bbwerwer__a.jpg",
-          width: 389,
-          height: 581,
-          title:"Street Photography - Kids",
-          href:"kids"          
-        },            
-        {
-          src: "/images/thumbnails/IMG_9819iiytrytryr.jpg",
-          width: 800,
-          height: 533,
-          title:"Street Photography - Candid and Unposed",
-          href:"street-photography"
-        },
-        {
-          src: "/images/video/home-the-goddess-of-hope.jpg",
-          width: 679,
-          height: 800,
-          title:"The Goddess of Hope - Original Music",
-          href:"music"          
-        },                                                      
-        {
-          src: "/images/thumbnails/DSC07512SMALL.jpg",
-          width: 640,
-          height: 427,
-          title:"Life on the Railroads - Photography Documerntary",
-          href:"life-on-the-railroads"          
-        },
-        {
-          src: "/images/thumbnails/paracas-skulls.jpg",
-          width: 640,
-          height: 850,
-          title:"Human History Revisited - YouTube Series",
-          href:"human-history-revisited"          
-        }        
-      ];
-      
-      const imageRenderer = ({
-        key,
-        photo,     
-      }) => {
-        
-        const handleOnClick = e => {
-          window.location.href = e.target.getAttribute('data-href');
-        };  
-  
+class Home extends Component {  
+    render() {          
+      const imageRenderer = ({ key, photo }) => {
         return (
-          <div 
-              key={key} >  
-                    <img                    
-                      alt={photo.title}
-                      data-href={photo.href}
-                      className="galleryImage"
-                      {...photo}
-                      onClick={handleOnClick}
-                    />                            
-          </div>
+          <a href={photo.href}>
+                <LazyLoadImage                    
+                  alt={photo.title}
+                  key={key}                  
+                  className="galleryImage"
+                  {...photo}
+                />                            
+          </a>
         );
       };
 
