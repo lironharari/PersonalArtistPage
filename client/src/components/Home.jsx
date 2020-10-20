a735835718dd2fb63c2de81e8fc24843844b541b
liron.harari <lironharari@gmail.com>
2020-10-19 20:48:59 -0500

    update

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -29,8 +29,8 @@ class Home extends Component {
               <title>liron harari</title>
               <meta property="og:title" content="Liron Harari" />
               <meta property="og:description" content="photography, documentary, drawings, and music." />
-              <meta property="og:image" content="//lironharari.herokuapp.com/images/Home.jpg" />
-              <meta property="og:url" content="//lironharari.herokuapp.com/" />              
+              <meta property="og:image" content="https://live.staticflickr.com/1946/43031971580_c816fbe714_z.jpg" />
+              <meta property="og:url" content="https://lironharari.herokuapp.com/" />              
               <meta property="og:site_name" content="Liron Harari" />                            
             </Helmet>
             {!isEmpty(photos) ? <Gallery photos={photos} renderImage={imageRenderer}></Gallery> : <div className="spinner"><CircularProgress /></div>}                                  
