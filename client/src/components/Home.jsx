9c8d1999ee255dd94a26c23b3f2de4cccd644dd4
liron.harari <lironharari@gmail.com>
2020-10-19 19:44:22 -0500

    update

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,6 +3,8 @@ import Gallery from "react-photo-gallery";
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import photos from './data/home.json'
 import { CircularProgress } from '@material-ui/core';
+import {Helmet} from "react-helmet";
+
 const { isEmpty } = require('lodash');				
 
 class Home extends Component {  
@@ -22,6 +24,15 @@ class Home extends Component {
 
       return (     
         <div className="homeGrid">
+            <Helmet>
+              <meta name="Description" content="photography, documentary, drawings, and music" />
+              <title>liron harari</title>
+              <meta property="og:title" content="Liron Harari" />
+              <meta property="og:description" content="photography, documentary, drawings, and music." />
+              <meta property="og:image" content="https://lironharari.herokuapp.com/images/thumbnails/_DSC3766.jpg" />
+              <meta property="og:url" content="https://lironharari.herokuapp.com/" />              
+              <meta property="og:site_name" content="Liron Harari" />                            
+            </Helmet>
             {!isEmpty(photos) ? <Gallery photos={photos} renderImage={imageRenderer}></Gallery> : <div className="spinner"><CircularProgress /></div>}                                  
         </div>
       );
